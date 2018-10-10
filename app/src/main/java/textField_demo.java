c7129e512b8a2e00622b266a04901a54a8d1e21e
Froilan Acas <acas.froilan@gmail.com>
2018-10-10 20:04:08 +0800

    labwork

---
+++ app/src/main/java/textField_demo.java
@@ -0,0 +1,7 @@
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import com.drawer.acas.fragments.R;
+
+public class textField_demo extends Fragment {
+
+}
---
+++ app/src/main/java/toggle_demo.java
@@ -0,0 +1,4 @@
+import android.support.v4.app.Fragment;
+
+public class toggle_demo extends Fragment {
+}
